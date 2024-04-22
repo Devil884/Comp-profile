f0270e71ab95b04050574e66b5591e873d113b5b
Devil884 <7376890457atul@gmail.com>
2024-04-22 15:29:11 +0530

    started mongodb

---
+++ backend/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const url ="mongodb+srv://7376890457atul:Atul123@cluster0.kogadqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(url)
+.then((result) =>{
+    console.log('connected to db');
+}).catch((err) =>{
+    console.log(err);
+});
