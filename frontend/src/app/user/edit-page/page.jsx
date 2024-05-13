c29f3254ebe2facd24935221a92e25fb9780fa80
Devil884 <7376890457atul@gmail.com>
2024-05-13 20:59:14 +0530

    category

--- frontend/src/app/user/edit-page/page.jsx
+++ frontend/src/app/user/edit-page/page.jsx
@@ -211,6 +211,26 @@ const EditPage = () => {
                                 htmlFor="af-account-email"
                                 className="inline-block text-sm text-gray-800 mt-2.5 dark:text-neutral-200"
                               >
+                                Categroy
+                              </label>
+                            </div>
+                            {/* End Col */}
+                            <div className="sm:col-span-9">
+                              <input
+                                id="category"
+                                onChange={handleChange}
+                                value={values.category}
+                                type="name"
+                                className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                placeholder="category"
+                              />
+                            </div>
+
+                            <div className="sm:col-span-3">
+                              <label
+                                htmlFor="af-account-email"
+                                className="inline-block text-sm text-gray-800 mt-2.5 dark:text-neutral-200"
+                              >
                                 Email
                               </label>
                             </div>
